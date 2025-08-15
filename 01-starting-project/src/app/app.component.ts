47f700489ec7358a5a8a65f422bba7beb2456bd2
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-15 21:57:57 +0200

    added a user

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from './header';
+import { HeaderComponent } from './header/header';
 
 @Component({
   selector: 'app-root',
