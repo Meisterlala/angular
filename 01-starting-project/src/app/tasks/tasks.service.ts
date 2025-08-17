b3ec567ccc667a8ba447131c6471e77563f62f3f
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-17 11:16:39 +0200

    added dependency injection

---
+++ 01-starting-project/src/app/tasks/tasks.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { newTask } from './new-task/new-task.model';
+
+@Injectable({ providedIn: 'root' })
+export class TasksService {
+  private tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ];
+
+  addTask(taskData: newTask, userId: string) {
+    this.tasks.push({
+      summary: taskData.summary,
+      title: taskData.title,
+      userId: userId,
+      dueDate: taskData.date,
+      // Pretty bad as an ID, but it works for the demo
+      id: new Date().getTime().toString(),
+    });
+  }
+
+  getUserTask(userId: string) {
+    return this.tasks.filter((task) => task.userId === userId);
+  }
+
+  removeTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+}
