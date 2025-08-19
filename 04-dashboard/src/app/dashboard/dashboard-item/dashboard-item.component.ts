11adb55d1e10b0156b137c9eacc93e6031e58893
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-19 23:32:18 +0200

    Refaktor

---
+++ 04-dashboard/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css',
+})
+export class DashboardItemComponent {
+  @Input({ required: true }) image!: { src: string; alt: string };
+
+  // "new" way to request an input
+  title = input.required<string>();
+}
