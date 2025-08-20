7f259c01e57a01d5e9d3815de7b69852a2ab6d5a
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-20 16:01:44 +0200

    Added new components

--- 04-dashboard/src/app/app.component.ts
+++ 04-dashboard/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TrafficComponent } from './dashboard/traffic/traffic.component';
-import { TicketsComponent } from './dashboard/tickets/tickets.component';
 import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
 
 @Component({
   selector: 'app-root',
