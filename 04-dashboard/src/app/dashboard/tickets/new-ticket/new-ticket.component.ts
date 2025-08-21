506304021c978e693479aa90d2daa3c71f0142cb
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-21 23:41:47 +0200

    use viewChild

--- 04-dashboard/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 04-dashboard/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent, ButtonComponent],
+  imports: [ControlComponent, ButtonComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  // 'form' is the template reference
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  private form = viewChild<HTMLFormElement>('form');
+
+  addTicket(title: string, text: string) {
+    this.form?.reset();
+  }
+}
