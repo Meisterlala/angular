334a98de224e3762de5559bfc9d88c01a74cc342
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-24 22:09:46 +0200

    Add more Signals

--- 09-rxjs/src/app/app.component.ts
+++ 09-rxjs/src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  effect,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // Signal to Observable
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
 
+  // Observable to Signal
+  interval$ = interval(1000);
+  interval = toSignal(this.interval$, { initialValue: 0 });
+
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    // effect(() => {
+    //   console.log(`Clicked button ${this.clickCount()} times.`);
+    // });
+  }
+
+  ngOnInit(): void {
+    // const s = interval(1000)
+    //   .pipe(map((val) => val * 2))
+    //   .subscribe({
+    //     next: (val) => {
+    //       console.log(val);
+    //     },
+    //   });
+    // this.destroyRef.onDestroy(() => {
+    //   s.unsubscribe();
+    // });
+
+    const s2 = this.clickCount$.subscribe({
+      next: (val) => {
+        console.log(val);
+      },
+    });
+    this.destroyRef.onDestroy(() => {
+      s2.unsubscribe();
+    });
+  }
+
+  onClick() {
+    this.clickCount.update((old) => old + 1);
+  }
 }
